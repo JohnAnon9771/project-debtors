42b05de304a2a6f72e2dc07c23384eaa2846670e
JohnAnon9771 <njoao97710@gmail.com>
2021-04-03 18:35:32 -0300

    :sparkles: FEAT(ui): add component aside for listening users

---
+++ src/components/Aside/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './style'
+
+export default function Aside(): JSX.Element {
+  return (
+    <S.Container>
+      <p>
+        Nenhum usuário. <br />
+        Por favor adicione alguns {':)'}
+      </p>
+    </S.Container>
+  )
+}
