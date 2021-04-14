dc189a7be767bb73ba4a97814bdd26376e222e91
JohnAnon9771 <njoao97710@gmail.com>
2021-04-13 22:53:19 -0300

    :sparkles: FEAT(component): add listining in component aside

--- src/components/Aside/style.ts
+++ src/components/Aside/style.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const Container = styled.aside`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 
@@ -9,7 +10,7 @@ export const Container = styled.aside`
   margin-right: 1.5rem;
 
   height: 65vh;
-  min-width: 200px;
+  min-width: 250px;
 
   background-color: rgba(255, 255, 255, 0.1);
   box-shadow: 0 25px 25px rgba(0, 0, 0, 0.1);
@@ -18,4 +19,19 @@ export const Container = styled.aside`
   border: 1px solid rgba(255, 255, 255, 0.5);
   border-right: 1px solid rgba(255, 255, 255, 0.2);
   border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+
+  font-size: 1.6rem;
+
+  .nothing-here {
+    text-align: center;
+  }
+
+  .users {
+    padding: 5px;
+    color: #fff;
+
+    border: 1px solid rgba(255, 255, 255, 0.5);
+    border-right: 1px solid rgba(255, 255, 255, 0.2);
+    border-bottom: 1px solid rgba(255, 255, 255, 0.2);
+  }
 `
